2a9420976952cd59548aaf0b8d37ee991598bfbe
kaushal8707 <kaushal.singh029@gmail.com>
2020-08-15 16:10:38 +0530

    Initial commit

---
+++ src/main/java/com/always/learner/DocServiceSD/DocServiceSdApplication.java
@@ -0,0 +1,13 @@
+package com.always.learner.DocServiceSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocServiceSdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocServiceSdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/always/learner/DocServiceSD/DocServiceSdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.learner.DocServiceSD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocServiceSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
